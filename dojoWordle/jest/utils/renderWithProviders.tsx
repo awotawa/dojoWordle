b3ed4281852aa6a6334db51c414f9980c7f811ae
Alexandre Wotawa <awotawa@gmail.com>
2022-12-06 18:02:50 +0100

    chore: create renderWithProviders component

---
+++ dojoWordle/jest/utils/renderWithProviders.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@emotion/react';
+import { ReactElement } from 'react';
+import { render } from '@testing-library/react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { theme } from '../../src/shared/services/theme/theme';
+import React from 'react';
+
+export const renderWithProviders = (component: ReactElement) => {
+  render(
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>{component}</ThemeProvider>
+    </SafeAreaProvider>
+  );
+};
