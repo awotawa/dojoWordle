fdbc9942a95a5f0cefa25acb4e83e03e04e1ea2f
Alexandre Wotawa <awotawa@gmail.com>
2022-12-07 16:39:52 +0100

    feat: add in keyboard

--- dojoWordle/src/app/Home.tsx
+++ dojoWordle/src/app/Home.tsx
@@ -20,8 +20,8 @@ export default function Home() {
 
   return (
     <GameViewContainer>
+      <LetterBox letter="" />
       <Game />
-      <LetterBox letter="A" />
     </GameViewContainer>
   );
 }
--- dojoWordle/src/components/Game.tsx
+++ dojoWordle/src/components/Game.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { WordleKeyboard } from './Keyboard';
 
 export function Game() {
   return (
     <View>
-      <Text>Hello Worldle ! </Text>
+      <WordleKeyboard />
     </View>
   );
 }
