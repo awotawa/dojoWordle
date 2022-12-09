ebdebc8eca3e2cc1c0bd038735e313027528fcca
Alexandre Wotawa <awotawa@gmail.com>
2022-12-09 11:28:23 +0100

    feat: add in possibility to add and delete letters

--- dojoWordle/src/components/Game.tsx
+++ dojoWordle/src/components/Game.tsx
@@ -3,18 +3,24 @@ import React, { useState } from 'react';
 import { WordleKeyboard } from './Keyboard';
 import { LetterBox, PossibleLetter, Validity } from './LetterBox';
 
+type Word = PossibleLetter[];
+
 export function Game() {
-  const [letter, setLetter] = useState<PossibleLetter>(null);
+  const [word, setWord] = useState<Word>(new Array(6).fill(null));
 
   return (
     <GameView>
-      <LetterBox letter={letter} validity={checkValidity(letter)} />
+      <WordleLetters>
+        {word.map((letter, index) => (
+          <LetterBox letter={letter} validity={'nofill'} key={index} />
+        ))}
+      </WordleLetters>
       <WordleKeyboard
         onKeyPress={(letter) => {
-          setLetter(letter);
+          addToWord({ letter, word, setWord });
         }}
         onDelPress={() => {
-          setLetter(null);
+          removeFromWord({ word, setWord });
         }}
       />
     </GameView>
@@ -24,6 +30,54 @@ export function Game() {
 const GameView = styled.View({
   alignItems: 'center',
 });
+
+const WordleLetters = styled.View({
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+});
+
+const findIndexOfFirstNullValueInWord = (array: Word) => {
+  const isNull = (element: PossibleLetter) => element === null;
+  return array.findIndex(isNull);
+};
+
+type addToWordProps = {
+  letter: PossibleLetter;
+  word: Word;
+  setWord: (word: Word) => void;
+};
+
+const addToWord = ({ letter, word, setWord }: addToWordProps): void => {
+  const newWord = [...word];
+
+  const indexOfFirstNull = findIndexOfFirstNullValueInWord(newWord);
+  if (indexOfFirstNull === -1) {
+    setWord(newWord);
+  }
+  newWord[indexOfFirstNull] = letter;
+
+  setWord(newWord);
+};
+
+type removeFromWordProps = {
+  word: Word;
+  setWord: (word: Word) => void;
+};
+
+const removeFromWord = ({ word, setWord }: removeFromWordProps) => {
+  const newWord = [...word];
+
+  const indexOfFirstNull = findIndexOfFirstNullValueInWord(newWord);
+  if (indexOfFirstNull === -1) {
+    newWord[5] = null;
+    setWord(newWord);
+  }
+  if (indexOfFirstNull === 0) {
+    setWord(newWord);
+  }
+  newWord[indexOfFirstNull - 1] = null;
+  setWord(newWord);
+};
 
 const checkValidity = (letter: PossibleLetter): Validity => {
   const validLetter = 'z';
